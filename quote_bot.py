a28dfde22a2aaad74882d183a33e23d9887ffa3d
nueron-brain <84946110+nueron-brain@users.noreply.github.com>
2021-06-10 13:33:26 +1200

    Update quote_bot.py

--- quote_bot.py
+++ quote_bot.py
@@ -5,6 +5,7 @@ import word_lists
 from word_lists import lists_noun
 from word_lists import lists_verb
 from word_lists import lists_adj
+from sentences import sentence_structure
 
 # Authenticate via OAuth
 client = pytumblr.TumblrRestClient(
@@ -22,17 +23,6 @@ generate = int(input('how many posts boss?'))
 # Creating a text post
 for i in range(0, generate):
     taglist = ['bot thoughts', 'shower thoughts', 'bot generated post', 'thoughts from the shower']
-    sentence_structure = [f"{random.choice(lists_noun)} is just a {random.choice(lists_noun)}",
-                          f'a {random.choice(lists_noun)} is like a {random.choice(lists_noun)} but if it was '
-                          f'{random.choice(lists_adj)}', f'if {random.choice(lists_noun)} had a '
-                                                         f'{random.choice(lists_noun)} then more {random.choice(lists_noun)}'
-                                                         f' would {random.choice(lists_verb)} it',
-                          f'life is a {random.choice(lists_verb)} {random.choice(lists_noun)}', f"you know when you're "
-                          f"{random.choice(lists_verb)}ing your {random.choice(lists_noun)}, "
-                         f"you're {random.choice(lists_verb)}ing your {random.choice(lists_noun)}", f'why do we have '
-                                    f'{random.choice(lists_adj)} {random.choice(lists_noun)} if we have {random.choice(lists_noun)}?',
-                          f'if you become a {random.choice(lists_noun)} then you are legally allowed to {random.choice(lists_verb)} '
-                          f'{random.choice(lists_noun)}s', f'{random.choice(lists_noun)} has big {random.choice(lists_noun)} energy']
     post = random.choice(sentence_structure).split(' ')
     for i in range(len(post)):
         word = post[i]
